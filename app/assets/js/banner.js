2ee82bcfbe80570cf5816bd5c8abb2dd51dc21d7
omar1190 <alxomar235@gmail.com>
2023-09-29 20:14:50 +0300

    t

--- app/assets/js/banner.js
+++
@@ -1,1 +0,0 @@
-document.querySelectorAll(".c-banner__close").forEach((function(e){e.addEventListener("click",(function(e){var t=e.target.parentNode;t.classList.add("collapse"),t.addEventListener("transitionend",(function(e){e.target===this&&this.remove()}))}))}));
